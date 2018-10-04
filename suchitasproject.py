9d198d81447f8ff9442c13903ef16968f60b5d83
suchitak13 <42971323+suchitak13@users.noreply.github.com>
2018-10-04 10:22:30 -0400

    Add files via upload

--- suchitasproject.py
+++ suchitasproject.py
@@ -3,3 +3,25 @@ Created on Sep 26, 2018
 
 @author: skatasani22
 '''
+name = input('please input your name:')
+phoneNumber = input('please input your phone number:')
+product = input('please enter the name of your product you would like to purchase:')
+price = float(input('please enter the price of the item')
+quantity = int(input('please enter quantity to purchase')
+
+subtotal = (price *quantity)
+tax = ((6/100)*total)
+total = tax+subtotal
+
+print()
+print(name)
+print()
+print(phoneNumber)
+print()
+print(productName)
+print()
+print(price)
+print()
+print(quantity)
+print()
+print(subtotal)
